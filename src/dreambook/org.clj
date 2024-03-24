089bcdb42c6e0839627b61f8a72308de9bca0c22
John Jacobsen <eigenhombre@gmail.com>
2024-03-23 21:29:42 -0500

    More cleanup

--- src/dreambook/org.clj
+++ src/dreambook/org.clj
@@ -1,34 +1,6 @@
 (ns dreambook.org
   (:require [clojure.string :as str]))
 
-(comment
-  (def dr-test "
-
-#+TITLE: Dreams
-#+DATE: <2017-08-29 Tue>
-#+OPTIONS: toc:nil num:nil
-
-* 1998
-
-** <1998-11-01 Sun>
-
-[Date is approximate.]
-
-I am about to give a talk. It will be about first guess reconstruction
-methods for AMANDA.  But I also have notes from Francis. They are....
-
-* 2000
-
-** <2000-08-01 Tue>
-
-Witches in a hospital (film).  A ritual – one of them says to me and
-
-** <2000-08-02 Wed>
-
-A few dreams – Looking at a world map and drinking milk with New
-Zealanders.
-"))
-
 (defn vec*
   "
   Like list*, but for vectors.  (vec* :a :b [:c :d]) => [:a :b :c :d].
